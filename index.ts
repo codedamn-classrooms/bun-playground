5fd7632d99c286e46f003787456127b8655b50f5
Pranav <pranav.techiegeek@gmail.com>
2023-09-11 17:21:05 +0530

    simplify playground

--- index.ts
+++ index.ts
@@ -1,25 +1,8 @@
 const index = Bun.file("./index.html");
-import fs from "fs"
-
-const staticFileServer = (request: Request) => {
-    const url = new URL(request.url);
-    const filePath = '.' + url.pathname;
-
-    if (url.pathname === '/') {
-        return new Response(index, {
-            headers: { 'content-type': 'text/html' }
-        })
-    }
-
-    if (fs.existsSync(filePath)) {
-        const file = Bun.file(filePath);
-        return new Response(file);
-    } else {
-        return new Response('Not Found', { status: 404 });
-    }
-};
 
 Bun.serve({
-    fetch: staticFileServer,
+    fetch(req: Request) {
+        return new Response(index)
+    },
     port: 1337
 })
